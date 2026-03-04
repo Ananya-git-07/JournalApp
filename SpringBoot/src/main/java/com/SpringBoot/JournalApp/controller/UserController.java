71313e4ba8195863c605ad8080db7ad818511a9e
Ananya Tiwari <ananyatiwari3004@gmail.com>
2026-03-04 17:16:03 +0000

    updated user controller &  journalentryservice

--- SpringBoot/src/main/java/com/SpringBoot/JournalApp/controller/UserController.java
+++ SpringBoot/src/main/java/com/SpringBoot/JournalApp/controller/UserController.java
@@ -16,23 +16,39 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping
-    public List<User> getAllUsers(){
-        return userService.getAll();
-    }
-    @PostMapping
-    public void createUser(@RequestBody User user){
-        userService.saveEntry(user);
+    @GetMapping("/get-users")
+    public ResponseEntity<?> getAllUsers(){
+        List<User> all = userService.getAll();
+        if(all != null && !all.isEmpty()){
+            return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("No Users", HttpStatus.NOT_FOUND);
     }
 
-    @PutMapping("/{userName}")
-    public ResponseEntity<?> updateUser(@RequestBody User user,@PathVariable String userName){
-        User userInDb=userService.findByUserName(user.getUserName());
-        if(userInDb !=null){
-            userInDb.setUserName(user.getUserName());
-            userInDb.setPassword(user.getPassword());
-            userService.saveEntry(userInDb);
+    @PostMapping("/add-user")
+    public ResponseEntity<?> postEntry(@RequestBody User entry){
+        try{
+            userService.saveEntry(entry);
+            return new ResponseEntity<>("Created", HttpStatus.CREATED);
         }
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("update-user/{u}")
+    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable String u){
+        User temp = userService.findByUserName(u);
+        if(temp!=null) {
+            if(user.getUserName() != null && !user.getUserName().isEmpty()){
+                temp.setUserName(user.getUserName());
+            }
+            if(user.getPassword() != null && !user.getPassword().isEmpty()){
+                temp.setPassword(user.getPassword());
+            }
+            userService.saveEntry(temp);
+            return new ResponseEntity<>(temp, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Not Found",HttpStatus.NOT_FOUND);
     }
 }
--- SpringBoot/src/main/java/com/SpringBoot/JournalApp/service/JournalEntryService.java
+++ SpringBoot/src/main/java/com/SpringBoot/JournalApp/service/JournalEntryService.java
@@ -1,32 +1,50 @@
 package com.SpringBoot.JournalApp.service;
 
 import com.SpringBoot.JournalApp.entity.JournalEntry;
+import com.SpringBoot.JournalApp.entity.User;
 import com.SpringBoot.JournalApp.repository.JournalEntryRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class JournalEntryService {
-        @Autowired
-        private JournalEntryRepository journalEntryRepository;
+    @Autowired
+    private JournalEntryRepository journalEntryRepository;
 
-        public void saveEntry(JournalEntry journalEntry){
-            journalEntryRepository.save(journalEntry);
-        }
+    @Autowired
+    private UserService userService;
 
-        public List<JournalEntry> getAll(){
-            return journalEntryRepository.findAll();
-        }
-
-        public Optional<JournalEntry> findById(Long id){
-          return journalEntryRepository.findById(id);
-        }
-
-        public void deleteById(Long id){
-            journalEntryRepository.deleteById(id);
-        }
+    public void saveEntry(JournalEntry journalEntry){
+        journalEntry.setDate(LocalDateTime.now());
+        journalEntryRepository.save(journalEntry);
     }
+
+    public void saveEntry(JournalEntry journalEntry, String userName){
+        User user = userService.findByUserName(userName);
+        journalEntry.setDate(LocalDateTime.now());
+        JournalEntry saved = journalEntryRepository.save(journalEntry);
+        user.getJournalEntries().add(saved);
+        userService.saveEntry(user);
+    }
+
+    public List<JournalEntry> getAll(){
+        return journalEntryRepository.findAll();
+    }
+
+    public Optional<JournalEntry> findById(ObjectId id){
+        return journalEntryRepository.findById(id);
+    }
+
+    public void deleteById(ObjectId id, String username){
+        User user = userService.findByUserName(username);
+        user.getJournalEntries().removeIf(x -> x.getId().equals(id));
+        userService.saveEntry(user);
+        journalEntryRepository.deleteById(id);
+    }
+}
 
